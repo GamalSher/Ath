aab05c9a2fe12518fae331394f7ce6de24e76c94
GamalSher <seriazdangamal474@gmail.com>
2022-11-28 17:34:59 +0600

    xz

---
+++ App.js
@@ -0,0 +1,44 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/Loginscreen";
+import HomeScreen from "./screens/HomeScreen";
+import Registration from "./Registration";
+const Stack = createNativeStackNavigator();
+// import { LogBox } from "react-native";
+// LogBox.ignoreAllLogs();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Login"
+          component={LoginScreen}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Registration"
+          component={Registration}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
